a5dd4accdfea6989e5b101630f95261f54085a9b
manselag123 <mansourelagouze@gmail.com>
2018-08-14 22:04:02 +0200

    investominiaWebsite
    investominiaWebsite

---
+++ Investomania/Controllers/NewsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Investomania.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: News
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
