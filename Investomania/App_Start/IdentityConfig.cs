61f850bc1f020dabea9804f65355aa88d3528c49
mansour <mansourelagouze@gmail.com>
2018-09-05 22:24:46 +0200

    mns

--- Investomania/App_Start/IdentityConfig.cs
+++ Investomania/App_Start/IdentityConfig.cs
@@ -16,6 +16,8 @@ namespace Investomania
 {
     public class EmailService : IIdentityMessageService
     {
+
+
         public Task SendAsync(IdentityMessage message)
         {
             // Plug in your email service here to send an email.
