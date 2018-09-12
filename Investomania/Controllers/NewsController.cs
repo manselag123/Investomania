68fb6d7bf8263c1ec59d11a591d2056630f2a0ab
mansour <mansourelagouze@gmail.com>
2018-09-12 06:03:07 +0200

    sdfdsafa

--- Investomania/Controllers/NewsController.cs
+++ Investomania/Controllers/NewsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Investomania.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,15 +9,29 @@ namespace Investomania.Controllers
 {
     public class NewsController : Controller
     {
+        private InvestomaniaEntities db = new InvestomaniaEntities();
         // GET: News
         public ActionResult Index()
         {
+            var news = db.News.OrderByDescending(e => e.creationDate).Take(100).ToList();
             return View();
         }
         [HttpGet]
         public ActionResult Create()
         {
+            return View(new New());
+        }
+        public ActionResult addPost()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Create(New newObj)
+        {
+            newObj.creationDate = DateTime.Now;
+            db.News.Add(newObj);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
